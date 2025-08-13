d117baf9bf68d3c17727cfdb80e11c14085d5edb
falakraisinghani <falakraisinghani@gmail.com>
2025-08-13 12:11:22 +0530

    Add files via upload

---
+++ MINtoHR.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+float num1,hour;
+printf("what minute to convert into hour:");
+scanf("%f",&num1);
+hour=num1/60;
+printf("%f minutes in hours is %f hours \n", num1,hour );
+
+}
+
