3db6534bed6693f4db986602c3b5d7b251461f9e
falakraisinghani <falakraisinghani@gmail.com>
2025-08-13 12:05:27 +0530

    Add files via upload

---
+++ HRtoMIN.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() {
+float num1,min;
+printf("what hour to convert into min:");
+scanf("%f",&num1);
+min=num1*60;
+printf("%f hours in minutes is %f \n", num1,min );
+
+}
