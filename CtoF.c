5e538732405dd9b3865b5142599e74abbd6d5afc
falakraisinghani <falakraisinghani@gmail.com>
2025-08-20 06:40:40 +0530

    CtoF.c

--- CtoF.c
+++ CtoF.c
@@ -4,6 +4,7 @@ void main (){
 float  cel,fah;
 printf("temperature to convert in fahrenheit : ");
 scanf("%f",&cel);
-fah=(9*cel/5)+32;
+fah=(9*cel)/5+32;
 printf("%f celsius is %f fahrenheit \n",cel,fah);
 }
+
