1062472aabb5e2ac20d5913c48ea6cf6cfae897f
falakraisinghani <falakraisinghani@gmail.com>
2025-09-03 11:43:16 +0530

    Add files via upload

---
+++ Netsalaryy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main() {
+ float gross_salary,allowance,deduction,net_salary;
+ printf("Enter gross salary : \n");
+ scanf("%f",&gross_salary);
+ if(gross_salary<10000)
+ { elseif(gross_salary>5000)
+ {
+  allowance=gross_salary*0.07;
+  deduction=gross_salary*0.02;
+
+                                }
+
+ }
+}
