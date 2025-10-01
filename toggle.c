1817554746f429e9db237de0313d2aff82e81ee9
falakraisinghani <falakraisinghani@gmail.com>
2025-10-01 12:38:01 +0530

    Add files via upload

---
+++ toggle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main () {
+char a[50];
+printf("Enter a word: \n");
+scanf("%s",&a);
+//char s[5]="&a";
+int i=0;
+while (a[i]!= '\0'){
+    if (a[i]>='a' && a[i]<='z'){
+        a[i]=a[i]-32;
+    }
+    else if (a[i]>='A' && a[i]<='Z'){
+        a[i]=a[i]+32;
+    }
+    i++;
+}
+printf("%s",a);
+}
